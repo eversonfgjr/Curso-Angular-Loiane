55c5abbc1cac49b1b0db3191fcbd634b92ea878a
eversonf <eversonfgjr@gmail.com>
2020-01-03 11:05:45 -0300

    Curso Angular #07: Introdução aos Serviços e Injeção de Dependência (DI)

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
+
 
 @Component({
   selector: 'app-cursos',
@@ -8,10 +10,12 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
-  cursos: string[] = ['Java', 'Angular'];
+  cursos: string[];
 
-  constructor() { 
+  constructor(private cursosService: CursosService) { 
     this.nomePortal = 'http://loiane.training';
+
+    this.cursos = cursosService.getCursos();
   }
 
   ngOnInit() {
