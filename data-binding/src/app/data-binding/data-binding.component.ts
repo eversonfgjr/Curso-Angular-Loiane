72db445f2c0b917b35b250e0decee9a5c475892b
eversonf <eversonfgjr@gmail.com>
2020-01-07 11:11:41 -0300

    Curso Angular #11: Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,9 +11,24 @@ export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com';
   urlImagem = 'http://lorempixel.com/400/200/nature/';
 
+  valorAtual:string  = '';
+  valorSalvo:string = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  botaoClicado() {
+    alert('botao clicado');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement> evento.target).value; 
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
   }
 
 }
