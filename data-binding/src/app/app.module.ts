8c1e197c30eb6af39e9f11ce196cae2aa0fffaba
Everson Junior <eversonfgjr@gmail.com>
2020-01-08 14:01:17 -0200

    Curso Angular #15: Ciclo de vida do Componente

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { FormsModule } from '@angular/forms';
+import { CicloComponent } from './ciclo/ciclo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    CicloComponent
   ],
   imports: [
     BrowserModule,
---
+++ data-binding/src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent implements OnInit {
+
+  @Input() valorInicial:number = 10;
+
+  constructor() { 
+    this.log('constructor');
+  }
+
+  ngOnChanges(){
+    this.log('ngOnChanges');
+  }
+
+  ngOnInit() {
+    this.log('ngOnInit');
+  }
+
+  ngDoCheck() {
+    this.log('ngDoCheck');
+  }
+
+  ngOnDestroy() {
+    this.log('ngOnDestroy');
+  }
+
+  private log(hook: string){
+    console.log(hook);
+  }
+
+
+
+}
